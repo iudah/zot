848caa2dd3574ef21fb628f004acb569ef50c1fa
iudah <olaolujude@gmail.com>
2025-12-16 02:00:46 +0100

    Disable custom allocator when AddressSanitizer is enabled
    Detect AddressSanitizer via __has_feature(address_sanitizer) and
    conditionally route zmalloc/zcalloc/zrealloc/zfree to libc allocators.
    This allows ASAN to correctly intercept allocations, preserving
    redzones and improving detection of memory errors while keeping the
    custom allocator for non-ASAN builds.

--- src/zot.c
+++ src/zot.c
@@ -5,20 +5,36 @@
 
 typedef uint64_t zsize;
 
+#if defined(__has_feature)
+#if __has_feature(address_sanitizer)
+#define Z_USE_ASAN 1
+#endif
+#endif
+
 void *zmalloc(const zsize size) {
-  // return malloc(size);
+#if Z_USE_ASAN
+  return malloc(size);
+#else
   return allocate(size);
+#endif
 }
+
 void *zcalloc(const zsize count, const zsize unit_size) {
+#if Z_USE_ASAN
+  return calloc(count, unit_size);
+#else
   if (!count || !unit_size)
     return NULL;
+
   uint64_t size = count * unit_size;
   /* check mul overflow */
   if (unit_size != size / count)
     return NULL;
+
   void *ptr = zmalloc(size);
   if (!ptr)
     return NULL;
+
   // Paginate loop because a single memset triggers UBSan on Android
   uintptr_t mem = (uintptr_t)ptr;
 #define MEMSET_LIMIT 65535
@@ -29,16 +45,24 @@ void *zcalloc(const zsize count, const zsize unit_size) {
   memset((void *)(mem + page), 0, size - page);
 
   return ptr;
+#endif
 }
 
 void *zrealloc(void *ptr, const zsize size) {
-  // return realloc(ptr, size);
+#if Z_USE_ASAN
+  return realloc(ptr, size);
+#else
   return reallocate(ptr, size);
+#endif
 }
 
 bool zfree(void *ptr) {
-  // return free(ptr);
+#if Z_USE_ASAN
+  free(ptr);
+  return true;
+#else
   return claim(ptr);
+#endif
 }
 
 int zthread_create(pthread_t *_Nonnull thread_ptr,
